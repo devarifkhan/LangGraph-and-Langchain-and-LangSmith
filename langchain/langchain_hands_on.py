fbbd4e7fcdf3751bebb1e8a929bd25b7c0a11c79
devarifkhan <arifcse209@gmail.com>
2026-01-30 16:33:17 +0600

    Add comments explaining usage of TextLoader and PyPDFLoader in example

--- langchain/langchain_hands_on.py
+++ langchain/langchain_hands_on.py
@@ -1,9 +1,11 @@
 from langchain_community.document_loaders import TextLoader, PyPDFLoader
 
+# load a text file
 loader = TextLoader('NeRF.txt')
 text_documents = loader.load()
 print(text_documents)
 
+# load a PDF file
 loader = PyPDFLoader('iso27001.pdf')
 docs = loader.load()
 print(docs)
