38392ffa2ff7dcd87bad19062e95b53da764c617
devarifkhan <arifcse209@gmail.com>
2026-01-31 12:05:40 +0600

    Update PDF file loaded in example to Python_Programming.pdf

--- langchain/langchain_hands_on.py
+++ langchain/langchain_hands_on.py
@@ -1,18 +1,9 @@
 import bs4
 from langchain_community.document_loaders import TextLoader, PyPDFLoader, WebBaseLoader
 
-# load a text file
-loader = TextLoader('NeRF.txt')
-text_documents = loader.load()
-print(text_documents)
-
 # load a PDF file
-loader = PyPDFLoader('iso27001.pdf')
+loader = PyPDFLoader('Python_Programming.pdf')
 docs = loader.load()
 print(docs)
 print("Page content: ",docs[0].page_content)
 
-# Web based loaders
-loader = WebBaseLoader(web_paths=["https://en.wikipedia.org/wiki/Artificial_intelligence"])
-loaded_web_docs= loader.load()
-print(loaded_web_docs)
