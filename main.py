85ff4f29e54e6433c1537b592213bd870a80e97a
devarifkhan <arifcse209@gmail.com>
2026-02-01 11:39:30 +0600

    Add search function to perform internet queries

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from dotenv import load_dotenv
 from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
+
+def search(query:str)->str:
+    """
+    Tool that searches over the internet
+    Args:
+        query (str): The query is searching for
+    Returns:
+        str: The search result
+    """
+    print(f"Searching for {query}")
+    return "Dhaka weather is sunny"
 
 def main():
     print("Hello from langchain agent")
