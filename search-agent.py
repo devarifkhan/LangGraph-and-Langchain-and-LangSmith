484d38d33dc5f678690b60fa242a6e113b9d63ac
devarifkhan <arifcse209@gmail.com>
2026-02-01 11:41:13 +0600

    Rename main.py to search-agent.py and update search function to use @tool decorator

--- search-agent.py
+++ search-agent.py
@@ -6,7 +6,9 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
-def search(query:str)->str:
+
+@tool
+def search(query: str) -> str:
     """
     Tool that searches over the internet
     Args:
@@ -17,8 +19,15 @@ def search(query:str)->str:
     print(f"Searching for {query}")
     return "Dhaka weather is sunny"
 
+
+llm = ChatGoogleGenerativeAI()
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+
+
 def main():
     print("Hello from langchain agent")
+
 
 if __name__ == "__main__":
     main()
